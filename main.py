a671891749dc83a4499448ba2c15ca1680cacddc
alshbg2001 <109930669+alshbg2001@users.noreply.github.com>
2022-08-12 23:12:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ bot = Client(
     bot_token='1128799285:AAH634XjJOUgU1CfeMYK35uU0GJOkAypiL4'
 )
 
+@bot.on_message(filters.regex('start'))
+def trply_to_hi(bot, message):
+    bot.send_message(message.chat.id, f"welcom is id : {message.text}")
+    
 @bot.on_message(filters.regex('id'))
 def trply_to_hi(bot, message):
     bot.send_message(message.chat.id, f"welcom is id : {message.chat.id}")
